1f792d6e36623bf5729199bc9f59855789ef7a6d
Seamus Keher <Seamus Keher@192.168.192.18>
2017-10-17 11:13:06 +0100

    Created a 'root' firstNode and added a second test

--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -2,6 +2,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import testLCA.LCA;
+import testLCA.Node;
+
 public class LCATest {
 
 	@Test
@@ -10,4 +13,12 @@ public class LCATest {
 		assertNull("The tree is empty: ", bt.getLCA(2,3));
 	}
 
+	@Test
+	public void emptyTreeNode() {
+		LCA bt = new LCA();
+		bt.firstNode = new Node(1);
+		assertEquals("Single node in the tree: ", 1, bt.getLCA(1, 0).data);
+		assertEquals("Single node in the tree: ", 1, bt.getLCA(0, 1).data);
+		assertEquals("Single node in the tree: ", 1, bt.getLCA(1, 1).data);
+	}
 }
