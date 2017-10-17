98d3e2894a086772251d303f68d1b63916bf3c76
Seamus Keher <Seamus Keher@192.168.192.18>
2017-10-17 17:26:48 +0100

    Added a third test. Also added some comments to explain methodology

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,19 +1,21 @@
-class Node {
-	int data;
-	Node left;
+class Node { //Node class will be used to create a binary tree
+	int data; //Node stores a value
+	Node left; 
 	Node right;
 
-	public Node(int data) {
+	public Node(int data) { //initialise
 		this.data = data;
 		left = null;
 		right = null;
 	}
 }
-public class LCA {
+public class LCA { //LCA class.  Lowest common ancestor will be found in here.
+	
+	Node firstNode; //create a root, called firstNode.
 
-	Node getLCA (int num1, int num2) {
+	Node getLCA (int num1, int num2) { //Num1 and Num2 are two integer values.  We will find the lowest common ancestor of these.
 		
-		return null;
+		return null; //return null for now. Write test cases first.
 	}
 
 }
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 
 public class LCATest {
 
-	@Test
+	@Test //Test to ensure null is returned when the tree is empty.
 	public void emptyTree() {
 		LCA bt = new LCA();
 		assertNull("The tree is empty: ", bt.getLCA(2,3));
 	}
 
 	@Test
-	public void oneNodeTree() {
+	public void oneNodeTree() { //test to ensure the LCA of a tree with a single node is the node itself i.e the root (firstNode).
 		LCA bt = new LCA();
 		bt.firstNode = new Node(1);
 		assertEquals("Single node in the tree: ", 1, bt.getLCA(1, 0).data);
@@ -20,10 +20,21 @@ public class LCATest {
 	}
 	
 	@Test
-	public void twoNodesTree () {
+	public void twoNodesTree () { //test to ensure the LCA of a tree with two nodes is returned correctly.
 		LCA bt = new LCA();
 		bt.firstNode = new Node(1);
 		bt.firstNode.left = new Node(2);
 		assertEquals("Two nodes tree: ", 1, bt.getLCA(1, 2).data);
+		assertEquals("Two nodes tree: ", 1, bt.getLCA(2, 1).data);
+	}
+	
+	@Test
+	public void unevenTree () { //test to ensure the LCA of an unbalanced, one sided tree is returned correctly.
+		LCA bt = new LCA();
+		bt.firstNode = new Node(1);
+		bt.firstNode.right = new Node(2);
+		bt.firstNode.right.right = new Node(3);
+		assertEquals("Uneven tree with three nodes: ", 2, bt.getLCA(2,3).data);	
+		assertEquals("Uneven tree with three nodes: ", 1, bt.getLCA(3, 1).data);
 	}
 }
