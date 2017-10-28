129f013559590b3d9283e2964ca7615364300d4b
Seamus Keher <Seamus Keher@192.168.192.18>
2017-10-28 20:33:09 +0100

    test commit to branch

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,3 +1,5 @@
+//Created a branch to work on the modification of the initial LCA problem to implement directed acyclic graphs.
+
 class Node { //Node class will be used to create a binary tree
 	int data; //Node stores a value
 	Node left; 
