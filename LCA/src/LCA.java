1cb4caf4fabe526f22b7959bac95de4ea19bb4cb
Seamus Keher <Seamus Keher@192.168.192.18>
2017-10-29 16:50:22 +0000

    Commented out the LCA of a binary tree code. Re-thinking how I will
    adjust to suit the Directed Acyclic Graph version of this problem.  Made
    some minor adjustments to the Node class. Removed left and right. Will
    now only need to keep track of children. Created a skeleton of one test.
    Will work on implementing it now.

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,56 +1,62 @@
+import java.util.ArrayList;
+
 //Created a branch to work on the modification of the initial LCA problem to implement directed acyclic graphs (DAG).
 
-class Node { //Node class will be used to create a binary tree
+class Node<T> { 
 	int data; //Node stores a value
-	Node left; 
-	Node right;
+	public ArrayList<Node<T>> listOfChildNodes; //keeps track of a nodes children
+	int child[];
 
 	public Node(int data) { //initialise
 		this.data = data;
-		left = null;
-		right = null;
+		listOfChildNodes = new ArrayList<Node<T>>();
+		child = null;
 	}
 }
+
+
 public class LCA { //LCA class. Lowest Common Ancestor will be found here.
 
-	Node firstNode; //create a root, called firstNode.
-
-	Node getLCA(int num1, int num2) { //Num1 and Num2 are two integer values.  We will find the lowest common ancestor of these.
-
-		return getLCA(firstNode, num1, num2);
-	}
-
-	Node getLCA(Node node, int num1, int num2) {
-		//The code below will find the LCA of two nodes recursively.
-		
-		if (node == null) { //Checks if the current node is null.
-			return null;
-		}
-		if(!numsPresent(firstNode, num1) || !numsPresent(firstNode, num2)) { //if one of the nodes isn't present, return null.
-			return null;
-		}
-		if (node.data == num1 || node.data == num2) { //checks if one of the numbers is equal to the node's value.
-			return node;
-		}
-
-		Node leftNode = getLCA(node.left, num1, num2); //left subtree
-		Node rightNode = getLCA(node.right, num1, num2); //right subtree.
-		
-		if (leftNode != null && rightNode != null) {
-			return node;
-		}
-
-		return (leftNode != null) ? leftNode : rightNode;
-		//if leftNode is not null, return leftNode. If leftNode is null, return rightNode.
-	}
+//	Node firstNode; //create a root, called firstNode.
+//
+//	Node getLCA(int num1, int num2) { //Num1 and Num2 are two integer values.  We will find the lowest common ancestor of these.
+//
+//		return getLCA(firstNode, num1, num2);
+//	}
+//
+//	Node getLCA(Node node, int num1, int num2) {
+//		//The code below will find the LCA of two nodes recursively.
+//		
+//		if (node == null) { //Checks if the current node is null.
+//			return null;
+//		}
+//		if(!numsPresent(firstNode, num1) || !numsPresent(firstNode, num2)) { //if one of the nodes isn't present, return null.
+//			return null;
+//		}
+//		if (node.data == num1 || node.data == num2) { //checks if one of the numbers is equal to the node's value.
+//			return node;
+//		}
+//
+//		Node leftNode = getLCA(node.left, num1, num2); //left subtree
+//		Node rightNode = getLCA(node.right, num1, num2); //right subtree.
+//		
+//		if (leftNode != null && rightNode != null) {
+//			return node;
+//		}
+//
+//		return (leftNode != null) ? leftNode : rightNode;
+//		//if leftNode is not null, return leftNode. If leftNode is null, return rightNode.
+//	}
+//	
+//	public boolean numsPresent (Node node, int num) { //check to see if a node is present in the tree.
+//		
+//		if (node == null) return false;
+//		if (node.data == num) return true;
+//		if (numsPresent(node.left, num)) return true;
+//		if (numsPresent(node.right, num)) return true;
+//		
+//		return false;
+//	}
 	
-	public boolean numsPresent (Node node, int num) { //check to see if a node is present in the tree.
-		
-		if (node == null) return false;
-		if (node.data == num) return true;
-		if (numsPresent(node.left, num)) return true;
-		if (numsPresent(node.right, num)) return true;
-		
-		return false;
-	}
+
 }
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -4,76 +4,81 @@ import org.junit.Test;
 
 public class LCATest {
 	
-	@Test //Ensures the constructor is working.
-	public void constructerTest () {
-		Node x = new Node(1);
-		assertNotNull("Testing if the constructer works ", x);
-	}
-
-	@Test //Test to ensure null is returned when the tree is empty.
-	public void emptyTree() {
-		LCA bt = new LCA();
-		assertNull("The tree is empty: ", bt.getLCA(2,3));
-	}
-
-	@Test //test to ensure the LCA of a tree with a single node is the node itself i.e the root (firstNode).
-	public void oneNodeTree() { 
-		LCA bt = new LCA();
-		bt.firstNode = new Node(1);
-		assertEquals("Single node in the tree: ", 1, bt.getLCA(1, 1).data);
+	@Test 
+	public void testNullInputs() {
+		
 	}
 	
-	@Test //test to ensure the LCA of a tree with two nodes is returned correctly.
-	public void twoNodesTree () { 
-		LCA bt = new LCA();
-		bt.firstNode = new Node(1);
-		bt.firstNode.left = new Node(2);
-		assertEquals("Two nodes tree: ", 1, bt.getLCA(1, 2).data);
-		assertEquals("Two nodes tree: ", 1, bt.getLCA(2, 1).data);
-	}
-	
-	@Test //test to ensure the LCA of an unbalanced, one sided tree is returned correctly.
-	public void unevenTree () { 
-		LCA bt = new LCA();
-		bt.firstNode = new Node(1);
-		bt.firstNode.right = new Node(2);
-		bt.firstNode.right.right = new Node(3);
-		assertEquals("Uneven tree with three nodes: ", 2, bt.getLCA(2,3).data);	
-		assertEquals("Uneven tree with three nodes: ", 1, bt.getLCA(3, 1).data);
-	}
-	
-	@Test //Test to ensure null is returned when we try to find the LCA of numbers that are not present in the tree.
-	public void testForNonExistentNodesInTree () { 
-		LCA bt = new LCA();
-		bt.firstNode = new Node(1);
-		bt.firstNode.left = new Node(2);
-		bt.firstNode.right = new Node(3);
-		bt.firstNode.left.left = new Node(4);
-		bt.firstNode.left.right = new Node(5);
-		bt.firstNode.right.right = new Node(6);
-		bt.firstNode.right.left = new Node(7);
-		assertNull("Searching for two nodes that are not in the tree: ", bt.getLCA(12, 19)); //fixed mistake here. Removed '.data'
-		assertNull("Searching for one node that is not in the tree: ", bt.getLCA(3, 10)); //found a glitch here. Assertion error
-		assertNull("Searching for one node that is not in the tree: ", bt.getLCA(56, 7));
-	}
-	
-	@Test //ensures the LCA of two nodes in a large tree is returned correctly.
-	public void largeTree () {
-		LCA bt = new LCA();
-		bt.firstNode = new Node(1);
-		bt.firstNode.left = new Node(2);
-		bt.firstNode.right = new Node(3);
-		bt.firstNode.left.left = new Node(4);
-		bt.firstNode.left.right = new Node(5);
-		bt.firstNode.right.right = new Node(6);
-		bt.firstNode.right.left = new Node(7);
-		bt.firstNode.left.left.left = new Node(8);
-		bt.firstNode.right.left.left = new Node(9);
-		bt.firstNode.right.left.right = new Node(10);
-		assertEquals("Many nodes in a tree: ", 1 ,bt.getLCA(1, 3).data);
-		assertEquals("Many nodes in a tree: ", 1 ,bt.getLCA(5, 1).data);
-		assertEquals("Many nodes in a tree: ", 2 ,bt.getLCA(5, 4).data);
-		assertEquals("Many nodes in a tree: ", 7 ,bt.getLCA(9, 10).data);
-		assertEquals("Many nodes in a tree: ", 3 ,bt.getLCA(6, 9).data);
-	}
+//	@Test //Ensures the constructor is working.
+//	public void constructerTest () {
+//		Node x = new Node(1);
+//		assertNotNull("Testing if the constructer works ", x);
+//	}
+//
+//	@Test //Test to ensure null is returned when the tree is empty.
+//	public void emptyTree() {
+//		LCA bt = new LCA();
+//		assertNull("The tree is empty: ", bt.getLCA(2,3));
+//	}
+//
+//	@Test //test to ensure the LCA of a tree with a single node is the node itself i.e the root (firstNode).
+//	public void oneNodeTree() { 
+//		LCA bt = new LCA();
+//		bt.firstNode = new Node(1);
+//		assertEquals("Single node in the tree: ", 1, bt.getLCA(1, 1).data);
+//	}
+//	
+//	@Test //test to ensure the LCA of a tree with two nodes is returned correctly.
+//	public void twoNodesTree () { 
+//		LCA bt = new LCA();
+//		bt.firstNode = new Node(1);
+//		bt.firstNode.left = new Node(2);
+//		assertEquals("Two nodes tree: ", 1, bt.getLCA(1, 2).data);
+//		assertEquals("Two nodes tree: ", 1, bt.getLCA(2, 1).data);
+//	}
+//	
+//	@Test //test to ensure the LCA of an unbalanced, one sided tree is returned correctly.
+//	public void unevenTree () { 
+//		LCA bt = new LCA();
+//		bt.firstNode = new Node(1);
+//		bt.firstNode.right = new Node(2);
+//		bt.firstNode.right.right = new Node(3);
+//		assertEquals("Uneven tree with three nodes: ", 2, bt.getLCA(2,3).data);	
+//		assertEquals("Uneven tree with three nodes: ", 1, bt.getLCA(3, 1).data);
+//	}
+//	
+//	@Test //Test to ensure null is returned when we try to find the LCA of numbers that are not present in the tree.
+//	public void testForNonExistentNodesInTree () { 
+//		LCA bt = new LCA();
+//		bt.firstNode = new Node(1);
+//		bt.firstNode.left = new Node(2);
+//		bt.firstNode.right = new Node(3);
+//		bt.firstNode.left.left = new Node(4);
+//		bt.firstNode.left.right = new Node(5);
+//		bt.firstNode.right.right = new Node(6);
+//		bt.firstNode.right.left = new Node(7);
+//		assertNull("Searching for two nodes that are not in the tree: ", bt.getLCA(12, 19)); //fixed mistake here. Removed '.data'
+//		assertNull("Searching for one node that is not in the tree: ", bt.getLCA(3, 10)); //found a glitch here. Assertion error
+//		assertNull("Searching for one node that is not in the tree: ", bt.getLCA(56, 7));
+//	}
+//	
+//	@Test //ensures the LCA of two nodes in a large tree is returned correctly.
+//	public void largeTree () {
+//		LCA bt = new LCA();
+//		bt.firstNode = new Node(1);
+//		bt.firstNode.left = new Node(2);
+//		bt.firstNode.right = new Node(3);
+//		bt.firstNode.left.left = new Node(4);
+//		bt.firstNode.left.right = new Node(5);
+//		bt.firstNode.right.right = new Node(6);
+//		bt.firstNode.right.left = new Node(7);
+//		bt.firstNode.left.left.left = new Node(8);
+//		bt.firstNode.right.left.left = new Node(9);
+//		bt.firstNode.right.left.right = new Node(10);
+//		assertEquals("Many nodes in a tree: ", 1 ,bt.getLCA(1, 3).data);
+//		assertEquals("Many nodes in a tree: ", 1 ,bt.getLCA(5, 1).data);
+//		assertEquals("Many nodes in a tree: ", 2 ,bt.getLCA(5, 4).data);
+//		assertEquals("Many nodes in a tree: ", 7 ,bt.getLCA(9, 10).data);
+//		assertEquals("Many nodes in a tree: ", 3 ,bt.getLCA(6, 9).data);
+//	}
 }
