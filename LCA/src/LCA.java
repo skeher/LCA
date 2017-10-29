c014cd423a5f7692df980b865518dccd908e769f
Seamus Keher <Seamus Keher@192.168.192.18>
2017-10-29 20:23:25 +0000

    Created 3 bare methods.  The isNotNull method seems to be functional
    according to the first test I wrote.  I will have to implement the
    isGraphAcyclic method and actually find the LCA. I have decided to use
    an ArrayList for this problem. This was suggested on an article on
    stackoverflow I read. I need to figure out how to implement this.

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -14,49 +14,32 @@ class Node<T> {
 	}
 }
 
-
 public class LCA { //LCA class. Lowest Common Ancestor will be found here.
-
-//	Node firstNode; //create a root, called firstNode.
-//
-//	Node getLCA(int num1, int num2) { //Num1 and Num2 are two integer values.  We will find the lowest common ancestor of these.
-//
-//		return getLCA(firstNode, num1, num2);
-//	}
-//
-//	Node getLCA(Node node, int num1, int num2) {
-//		//The code below will find the LCA of two nodes recursively.
-//		
-//		if (node == null) { //Checks if the current node is null.
-//			return null;
-//		}
-//		if(!numsPresent(firstNode, num1) || !numsPresent(firstNode, num2)) { //if one of the nodes isn't present, return null.
-//			return null;
-//		}
-//		if (node.data == num1 || node.data == num2) { //checks if one of the numbers is equal to the node's value.
-//			return node;
-//		}
-//
-//		Node leftNode = getLCA(node.left, num1, num2); //left subtree
-//		Node rightNode = getLCA(node.right, num1, num2); //right subtree.
-//		
-//		if (leftNode != null && rightNode != null) {
-//			return node;
-//		}
-//
-//		return (leftNode != null) ? leftNode : rightNode;
-//		//if leftNode is not null, return leftNode. If leftNode is null, return rightNode.
-//	}
-//	
-//	public boolean numsPresent (Node node, int num) { //check to see if a node is present in the tree.
-//		
-//		if (node == null) return false;
-//		if (node.data == num) return true;
-//		if (numsPresent(node.left, num)) return true;
-//		if (numsPresent(node.right, num)) return true;
-//		
-//		return false;
-//	}
 	
+	Node root;
+	
+	//returns an 
+	public static Node getLCA(ArrayList<Node> graph, Node node1, Node node2) {
+		
+		if (!isNotNull(graph, node1, node2) && !isGraphAcyclic()) {
+			return null;
+		} else {
+			//ToDo: find lca
+		}
+		return null;
+	}
+
+	//makes sure no inputs are null.
+	public static boolean isNotNull (ArrayList<Node> graph, Node node1, Node node2) {
+		if (graph == null) return false;
+		if (node1 == null || node2 == null) return false;
+		if (!graph.contains(node1) || !graph.contains(node2)) return false;
+		return true;
+	}
+	
+	public static boolean isGraphAcyclic () {
+		return false;
+		
+	}
 
 }
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -1,11 +1,28 @@
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class LCATest {
 	
 	@Test 
 	public void testNullInputs() {
+		assertNull("null inputs: ", LCA.getLCA(null, null, null));
+		Node dublin = new Node(1);
+		assertNull("null inputs: ", LCA.getLCA(null, dublin, null));
+		assertNull("null inputs: ", LCA.getLCA(null, null, dublin));
+		Node kildare = new Node(2);
+		assertNull("null inputs: ", LCA.getLCA(null, dublin, kildare));
+		assertNull("null inputs: ", LCA.getLCA(null, kildare, dublin));
+		
+		ArrayList<Node> counties = new ArrayList<Node>();
+		assertNull("null inputs: ", LCA.getLCA(counties, null, null));
+		assertNull("null inputs: ", LCA.getLCA(counties, dublin, null));
+		assertNull("null inputs: ", LCA.getLCA(counties, null, kildare));
+		assertNull("null inputs: ", LCA.getLCA(counties, dublin, kildare));
+	}
+	
+	@Test
+	public void testSingleNode() {
 		
 	}
 	
