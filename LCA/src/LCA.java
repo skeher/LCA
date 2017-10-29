012089c586d41ffe906e24661cb20e178f9d4262
Seamus Keher <Seamus Keher@192.168.192.18>
2017-10-29 21:56:34 +0000

    Attempted to implement is Acyclic function. Haven't tested it yet.
    Will write test for this next.

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -15,13 +15,13 @@ class Node<T> {
 }
 
 public class LCA { //LCA class. Lowest Common Ancestor will be found here.
-	
+
 	Node root;
-	
+
 	//returns an 
 	public static Node getLCA(ArrayList<Node> graph, Node node1, Node node2) {
-		
-		if (!isNotNull(graph, node1, node2) && !isGraphAcyclic()) {
+
+		if (!isNotNull(graph, node1, node2) && !isGraphAcyclic(graph)) {
 			return null;
 		} else {
 			//ToDo: find lca
@@ -36,10 +36,27 @@ public class LCA { //LCA class. Lowest Common Ancestor will be found here.
 		if (!graph.contains(node1) || !graph.contains(node2)) return false;
 		return true;
 	}
-	
-	public static boolean isGraphAcyclic () {
-		return false;
-		
-	}
 
+	public static boolean isGraphAcyclic (ArrayList<Node> graph) {
+		if (graph == null) return true; 
+		for (int i = 0; i < graph.size(); i ++) {
+			boolean graphHasACycle = false;
+			ArrayList<Node> checked = new ArrayList<Node>();
+			Node index = graph.get(i);
+			ArrayList<Node> listToCompare = new ArrayList<Node>();
+			graphHasACycle = isGraphAcyclic(graph,listToCompare,checked,graphHasACycle,index);
+			if (graphHasACycle) return false;
+		}
+		return true;
+	}
+	
+	private static boolean isGraphAcyclic(ArrayList<Node> graph, ArrayList<Node> listToCompare, ArrayList<Node> checked, boolean graphHasACycle, Node index) {
+		for (int i = 0; i < index.listOfChildNodes.size(); i ++) {
+			Node currentNode = (Node) index.listOfChildNodes.get(i);
+			if (!checked.contains(currentNode)) isGraphAcyclic(graph, listToCompare, checked, graphHasACycle, index);
+			else if (listToCompare.contains(currentNode)) return true;
+		}
+		listToCompare.remove(index);
+		return graphHasACycle;
+	}
 }
